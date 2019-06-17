7f471e6ef43afe3be193349ac6003edc0f8681d3
jiangxiaocai <jiangxiaocai@cryptogorilla.io>
2019-06-17 15:28:40 +0800

    add objc4-750

---
+++ runtime/objc4-750/debug-objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  debug-objc
+//
+//  Created by Closure on 2018/12/4.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
